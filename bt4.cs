65c968e6a0eb92d8f31c49de3822ce1a0072b334
qloc94 <qloc9424@gmail.com>
2024-12-29 14:33:27 +0700

    Add files via upload

---
+++ bt4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using baitap4;
+using Demo;
+
+namespace bt4
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new dta_Form());
+        }
+    }
+}
